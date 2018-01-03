36a0b8259dc0ad13d90a85445fbc8f4649e9a7e6
Matthew Scott <matt@11craft.com>
2018-01-03 10:15:14 -0600

    Move tr invocation to a var to explain its purpose better

--- diff2.sh
+++ diff2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-yarn dumpchunks "$@" 2>&1 | tr -cd '\11\12\15\40-\176' > /tmp/1.txt
-yarn loadsavedump "$@" 2>&1 | tr -cd '\11\12\15\40-\176' > /tmp/2.txt
+asciionly="tr -cd '\11\12\15\40-\176'"
+yarn dumpchunks "$@" 2>&1 | ${asciionly} > /tmp/1.txt
+yarn loadsavedump "$@" 2>&1 | ${asciionly} > /tmp/2.txt
 diff -y -W160 /tmp/1.txt /tmp/2.txt
